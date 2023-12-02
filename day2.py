4bfe57115a39827e3d132db3a0141763a484b171
Kim de Bie <kimdebie@users.noreply.github.com>
2023-12-02 13:57:01 +0000

    day2

---
+++ day2.py
@@ -0,0 +1,43 @@
+import re 
+
+from utils import read_input
+
+
+limits = {'red': 12, 'green': 13, 'blue': 14}
+
+def part1(games):
+
+    valid_games = 0
+
+    for i, game in enumerate(games):
+        game_valid = True
+        for color, limit in limits.items():
+            for match in re.findall(r'\d+ ' + color, game):
+                if int(match.split(' ')[0]) > limit:
+                    game_valid = False
+        if game_valid:
+            valid_games += i+1
+
+    print(valid_games)
+
+
+def part2(games):
+
+    games_sum = 0
+    
+    for game in games:
+        game_product = 1
+        for color in limits.keys():
+            game_product *= max(
+                int(match.split(' ')[0])
+                for match in re.findall(r'\d+ ' + color, game)
+            )
+        games_sum += game_product
+
+    print(games_sum)
+
+
+if __name__ == '__main__':
+    file_input = read_input('inputs/day2.txt')
+    part1(file_input)
+    part2(file_input)
--- utils.py
+++ utils.py
@@ -3,5 +3,5 @@ from typing import List
 
 def read_input(file_path: str) -> list[str]:
     with open(file_path, 'r') as f:
-        lines = [line.rstrip() for line in f][:-1]
+        lines = [line.rstrip() for line in f]
     return lines
