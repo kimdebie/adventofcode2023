5d060e7ec253c1cdccc4872d0f2866b790f80b66
Kim de Bie <kimdebie@users.noreply.github.com>
2023-12-10 09:16:18 +0000

    day9

--- day9.py
+++ day9.py
@@ -2,22 +2,28 @@ import re
 
 from utils import read_input
 
-def part1(sequences):
+def part12(sequences, end=True):
 
     extrapolations = 0
 
     for sequence in sequences:
-        sequence = [int(i) for i in re.findall(r'\d+', sequence)]
+        sequence = [int(i) for i in re.findall(r'-?\d+', sequence)]
         layers = [sequence]
 
-        while not sum(bool(i) for i in layers[-1]) == 0:
+        while not sum(i==0 for i in layers[-1]) == len(layers[-1]):
             differences = [layers[-1][i+1]-layers[-1][i] for i in range(len(layers[-1])-1)]
             layers.append(differences)
         layers[-1].append(0)
+
         for l in range(len(layers)-2, -1, -1):
-            last_digit = layers[l][-1] + layers[l+1][-1]
-            layers[l].append(last_digit)
-        extrapolations += layers[0][-1]
+            if end:
+                new_digit = layers[l][-1] + layers[l+1][-1]
+                layers[l].append(new_digit)
+            else:
+                new_digit = layers[l][0] - layers[l+1][0]
+                layers[l] = [new_digit] + layers[l]
+        extrapolations += new_digit
+
 
     print(extrapolations)
 
@@ -26,4 +32,5 @@ def part1(sequences):
 
 if __name__ == '__main__':
     file_input = read_input('inputs/day9.txt')
-    part1(file_input)
+    part12(file_input)
+    part12(file_input, end=False)
